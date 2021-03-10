28eb5930c645dba156790e9833e6890e1ac7a2e1
Aibek Q <aq@maq.kz>
2021-03-10 13:52:44 +0600

    update files.

--- index.js
+++ index.js
@@ -1,2 +1,77 @@
-"use strict";function _classCallCheck(a,b){if(!(a instanceof b))throw new TypeError("Cannot call a class as a function")}function _defineProperties(a,b){for(var c,d=0;d<b.length;d++)c=b[d],c.enumerable=c.enumerable||!1,c.configurable=!0,"value"in c&&(c.writable=!0),Object.defineProperty(a,c.key,c)}function _createClass(a,b,c){return b&&_defineProperties(a.prototype,b),c&&_defineProperties(a,c),a}var RNumber=/*#__PURE__*/function(){function a(){_classCallCheck(this,a)}return _createClass(a,null,[{key:"init",value:function init(){a.setEvents(),window.addEventListener("beforeunload",function(a){a.preventDefault(),a.returnValue=""}),document.addEventListener("keydown",function(b){b.ctrlKey&&"Enter"===b.key&&a.generate()})}},{key:"generate",value:function generate(){var b=a.specialSymbols()+a.getSymbols();b=a.shuffle(b),a.string=b.join(""),a.showSymbols()}},{key:"showSymbols",value:function showSymbols(){var b,c=document.getElementById("lower"),d=document.getElementById("original");c&&d&&(b=a.string,c.value=b.toLowerCase(),d.value=b,a.setSymbolsLength(b))}},{key:"specialSymbols",value:function specialSymbols(){var a=document.querySelectorAll("#symbols input[type=checkbox]:checked"),b="";if(a&&a.length)for(var c,d=0;d<a.length;d++)c=a[d],b+=c.parentNode.textContent;return b}},{key:"shuffle",value:function shuffle(c){for(var d=Math.floor,e=c.length,f=[],g=0;g<e;g++)f.push(c[g]);for(var h=0;h<e;h++){var j=d(Math.random()*e),a=d(Math.random()*e);j===a&&(0<j?j--:0===a&&a++);var b=f[j];f[j]=f[a],f[a]=b}return f}},{key:"getSymbols",value:function getSymbols(){var b=document.getElementById("abc"),c=document.getElementById("abcLength"),d=document.getElementById("ABC"),e=document.getElementById("ABCLength"),f=document.getElementById("numbers"),g=document.getElementById("numbersLength");return b&&c&&(a.abc=b,a.abcLength=c,b.querySelector("input[type=checkbox]:checked")?(b=this.shuffle(b.innerText.trim()),b.length=c.value):b=[],a.eventOnChange(c)),d&&e&&(a.ABC=d,a.ABCLength=e,d.querySelector("input[type=checkbox]:checked")?(d=this.shuffle(d.innerText.trim()),d.length=e.value):d=[],a.eventOnChange(e)),f&&g&&(a.numbers=f,a.numbersLength=g,f.querySelector("input[type=checkbox]:checked")?(f=this.shuffle(f.innerText.trim()),f.length=g.value):f=[],a.eventOnChange(g)),b.join("")+d.join("")+f.join("")}},{key:"setEvents",value:function setEvents(){var b=document.getElementById("symbols");b&&b.addEventListener("click",function(b){var c=b.target;switch(c.tagName){case"LABEL":case"INPUT":a.generate();break;default:b.preventDefault();}});var c=document.querySelector("button");c&&(c.addEventListener("click",a.generate),c.click());var d=document.querySelector("#templates");d&&d.addEventListener("click",function(b){var c=b.target;switch(c.tagName){case"INPUT":a.setTemplate(c.dataset.template),a.generate();/*default:
-                        event.preventDefault();*/}})}},{key:"setTemplate",value:function setTemplate(b){var c;switch(+b){case 1:a.abc&&(a.abc.checked=!0),a.abcLength&&(a.abcLength.value=3),a.ABC&&(a.ABC.checked=!0),a.ABC&&(a.ABCLength.value=3),a.numbers&&(a.numbers.checked=!0),a.numbersLength&&(a.numbersLength.value=3),c=document.querySelectorAll("#symbols input[type=\"checkbox\"]"),c&&c.length&&(c[2].checked=!0,c[3].checked=!0,c[4].checked=!0,c[10].checked=!0);break;case 2:a.abc&&(a.abc.checked=!0),a.abcLength&&(a.abcLength.value=11),a.ABC&&(a.ABC.checked=!0),a.ABC&&(a.ABCLength.value=11),a.numbers&&(a.numbers.checked=!0),a.numbersLength&&(a.numbersLength.value=11),c=document.querySelectorAll("#symbols input[type=\"checkbox\"]"),c&&c.length&&(c[2].checked=!0,c[3].checked=!0,c[4].checked=!0,c[10].checked=!0);}}},{key:"eventOnChange",value:function eventOnChange(b){b.addEventListener("change",a.generate)}},{key:"setSymbolsLength",value:function setSymbolsLength(a){var b=document.getElementById("length");b&&(b.innerText=a.length)}}]),a}();RNumber.string="",document.addEventListener("DOMContentLoaded",RNumber.init);
+"use strict";
+
+function i() {
+}
+
+i.init = function () {
+    i.t(), window.addEventListener("beforeunload", function (c) {
+        c.preventDefault(), c.returnValue = ""
+    }), document.addEventListener("keydown", function (c) {
+        if ("Enter" === c.key) return i.o();
+        if ("c" !== c.key) {
+            if ("q" === c.key || "t" === c.key) {
+                var t = document.getElementsByName("radio");
+                return "q" === c.key && (t[0].checked = !0, i.u(1)), "t" === c.key && (t[1].checked = !0, i.u(2)), i.o()
+            }
+        } else {
+            t = document.getElementById("O");
+            t && (t.select(), t.setSelectionRange(0, 1e3), document.execCommand("copy"), c.preventDefault())
+        }
+    })
+}, i.o = function () {
+    var c = i.i() + i.m(), c = i.h(c);
+    i.k = c.join(""), i.p()
+}, i.p = function () {
+    var c, t = document.getElementById("L"), n = document.getElementById("O");
+    t && n && (c = i.k, t.value = c.toLowerCase(), n.value = c, i.v(c))
+}, i.i = function () {
+    var c = document.querySelectorAll("#G input[type=checkbox]:checked"), t = "";
+    if (c && c.length) for (var n = 0; n < c.length; n++) t += c[n].parentNode.textContent;
+    return t
+}, i.h = function (c) {
+    for (var t = Math.floor, n = c.length, e = [], o = 0; o < n; o++) e.push(c[o]);
+    for (var u = 0; u < n; u++) {
+        var i = t(Math.random() * n), d = t(Math.random() * n);
+        i === d && (0 < i ? i-- : 0 === d && d++);
+        var r = e[i];
+        e[i] = e[d], e[d] = r
+    }
+    return e
+}, i.m = function () {
+    var c = document.getElementById("A"), t = document.getElementById("b"), n = document.getElementById("C"),
+        e = document.getElementById("d"), o = document.getElementById("E"), u = document.getElementById("f");
+    return c && t && (i.l = c, i.G = t, c.querySelector("input[type=checkbox]:checked") ? (c = this.h(c.innerText.trim())).length = t.value : c = [], i.L(t)), n && e && (i.M = n, i.I = e, n.querySelector("input[type=checkbox]:checked") ? (n = this.h(n.innerText.trim())).length = e.value : n = [], i.L(e)), o && u && (i.O = o, i.q = u, o.querySelector("input[type=checkbox]:checked") ? (o = this.h(o.innerText.trim())).length = u.value : o = [], i.L(u)), c.join("") + n.join("") + o.join("")
+}, i.t = function () {
+    var c = document.getElementById("G");
+    c && c.addEventListener("click", function (c) {
+        switch (c.target.tagName) {
+            case"LABEL":
+            case"INPUT":
+                i.o();
+                break;
+            default:
+                c.preventDefault()
+        }
+    });
+    c = document.querySelector("button");
+    c && (c.addEventListener("click", i.o), c.click());
+    c = document.querySelector("#H");
+    c && c.addEventListener("click", function (c) {
+        c = c.target;
+        "INPUT" === c.tagName && (i.u(c.dataset.A), i.o())
+    })
+}, i.u = function (c) {
+    var t;
+    switch (+c) {
+        case 1:
+            i.l && (i.l.checked = !0), i.G && (i.G.value = 3), i.M && (i.M.checked = !0), i.M && (i.I.value = 3), i.O && (i.O.checked = !0), i.q && (i.q.value = 3), (t = document.querySelectorAll('#G input[type="checkbox"]')) && t.length && (t[2].checked = !0, t[3].checked = !0, t[4].checked = !0, t[10].checked = !0);
+            break;
+        case 2:
+            i.l && (i.l.checked = !0), i.G && (i.G.value = 11), i.M && (i.M.checked = !0), i.M && (i.I.value = 11), i.O && (i.O.checked = !0), i.q && (i.q.value = 11), (t = document.querySelectorAll('#G input[type="checkbox"]')) && t.length && (t[2].checked = !0, t[3].checked = !0, t[4].checked = !0, t[10].checked = !0)
+    }
+}, i.L = function (c) {
+    c.addEventListener("change", i.o)
+}, i.v = function (c) {
+    var t = document.getElementById("I");
+    t && (t.innerText = c.length)
+}, i.k = "", document.addEventListener("DOMContentLoaded", i.init);
